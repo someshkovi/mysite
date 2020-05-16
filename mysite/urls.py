4a63ac2e882790e225515c7247830d5078c89f20
Somesh Kovi <someshkovi@gmai.com>
2020-05-16 16:28:56 +0530

    removed catalog settings

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,9 +19,3 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
-from django.urls import include
-
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
-]
