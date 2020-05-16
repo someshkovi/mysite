06609cb5cfb4519c58e959a38f6b44226991400f
Somesh Kovi <someshkovi@gmai.com>
2020-05-16 15:35:46 +0530

    added postgres db

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgressql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'iambatman',
+        'HOST': '10.66.43.18',
+        'PORT': '5432'
     }
 }
 
