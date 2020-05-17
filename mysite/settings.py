2eb287bcbccf0f848f2140c71ec5a89b9823d3cb
Somesh Kovi <someshkovi@gmai.com>
2020-05-17 23:25:43 +0530

    created polls app till part 4

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
